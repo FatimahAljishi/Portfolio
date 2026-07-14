7a81bca4b9add0313c852a01b8a52b3a5585e3ce
s2716746 <s2716746@ed.ac.uk>
2026-07-14 13:58:27 +0300

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Window from "./components/Window/Window";
 import Desktop from "./components/Desktop/Desktop";
 import Taskbar from "./components/Taskbar/Taskbar";
 import computerIcon from "./assets/computer.png";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return <Desktop />;
