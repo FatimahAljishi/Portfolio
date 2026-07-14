8faa46c2f90b995e7287c94aa2ad314e7288fc62
s2716746 <s2716746@ed.ac.uk>
2026-07-14 13:55:13 +0300

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Window from "./components/Window/Window";
 import Desktop from "./components/Desktop/Desktop";
 import Taskbar from "./components/Taskbar/Taskbar";
 import computerIcon from "./assets/computer.png";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return <Desktop />;
